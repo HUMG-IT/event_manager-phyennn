ead58f3af62b72e0154b5c79e4a1f52b39e15bf0
phyennn <phyen13112003@gmail.com>
2024-11-07 22:06:17 +0700

    nopbai

---
+++ lib/event/event_detail_view.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'event_model.dart';
+import 'event_service.dart';
+
+///Màn hình chi tiết sự kiện cho phép thêm mới/ cập nhật
+class EventDetailView extends StatefulWidget {
+  final EventModel event;
+  const EventDetailView({super.key, required this.event});
+
+  @override
+  State<EventDetailView> createState() => _EventDetailView();
+}
+
+class _EventDetailView extends State<EventDetailView> {
+  final subjectController = TextEditingController();
+  final notesController = TextEditingController();
+  final eventService = EventService();
+
+  @override
+  void initState() {
+    super.initState();
+    subjectController.text = widget.event.subject;
+    notesController.text = widget.event.notes ?? '';
+  }
+
+  Future<void> _pickDateTime({required bool isStart}) async {
+    //HIện hộp thoại chọn ngày
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: isStart ? widget.event.startTime : widget.event.endTime,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null) {
+      if (!mounted) return;
+      final pickedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(
+          isStart ? widget.event.startTime : widget.event.endTime,
+        ),
+      );
+
+      if (pickedTime != null) {
+        setState(() {
+          final newDateTime = DateTime(pickedDate.year, pickedDate.month,
+              pickedDate.day, pickedTime.hour, pickedDate.minute);
+          if (isStart) {
+            widget.event.startTime = newDateTime;
+            if (widget.event.startTime.isAfter(widget.event.endTime)) {
+              //Tự thiết lập endTime 1 tiếng sau startTime
+              widget.event.endTime =
+                  widget.event.startTime.add(const Duration(hours: 1));
+            }
+          } else {
+            widget.event.endTime = newDateTime;
+          }
+        });
+      }
+    }
+  }
+
+  Future<void> _saveEvent() async {
+    widget.event.subject = subjectController.text;
+    widget.event.notes = notesController.text;
+    await eventService.saveEvent(widget.event);
+    if (!mounted) return;
+    Navigator.of(context).pop(true); //Trở về màn hình trước đó
+  }
+
+  Future<void> _deleteEvent() async {
+    await eventService.deleteEvent(widget.event);
+    if (!mounted) return;
+    Navigator.of(context).pop(true); //Trở về màn hình trước đó
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text(widget.event.id == null ? 'Thêm sự kiện' : 'Chi tiết sự kiện'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: subjectController,
+                decoration: const InputDecoration(labelText: 'Tên sự kiện'),
+              ),const SizedBox(height: 16),
+              ListTile(
+                title: const Text('Sự kiện cả ngày'),
+                trailing: Switch(
+                    value: widget.event.isAllDay,
+                    onChanged: (value) {
+                      setState(() {
+                        widget.event.isAllDay = value;
+                      });
+                    }),
+              ),
+              if (!widget.event.isAllDay) ...[
+                const SizedBox(height: 16),
+                ListTile(
+                  title:
+                      Text('Bắt đầu: ${widget.event.formatedStartTimeString}'),
+                  trailing: const Icon(Icons.calendar_today_outlined),
+                  onTap: () => _pickDateTime(isStart: true),
+                ),
+                const SizedBox(height: 16),
+                ListTile(
+                  title:
+                      Text('Kết thúc: ${widget.event.formatedEndTimeString}'),
+                  trailing: const Icon(Icons.calendar_today_outlined),
+                  onTap: () => _pickDateTime(isStart: false),
+                ),
+                TextField(
+                  controller: notesController,
+                  decoration:
+                      const InputDecoration(labelText: 'Ghi chú sự kiện'),
+                  maxLines: 3,
+                ),
+                const SizedBox(height: 24),
+              ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  //CHỉ hiện hiển thị nút xoá nếu không phải sự kiện mới
+                  if (widget.event.id != null)
+                    FilledButton.tonalIcon(
+                        onPressed: _deleteEvent,
+                        label: const Text("Xoá sự kiện")),
+                  FilledButton.icon(
+                      onPressed: _saveEvent, label: const Text("Lưu sự kiện"))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/event/event_view.dart
@@ -0,0 +1,133 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:event_manager/event/event_data_source.dart';
+import 'package:event_manager/event/event_detail_view.dart';
+import 'package:event_manager/event/event_model.dart';
+import 'package:event_manager/event/event_service.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class EventView extends StatefulWidget {
+  const EventView({super.key});
+
+  @override
+  State<EventView> createState() => _EventViewState();
+}
+
+class _EventViewState extends State<EventView> {
+  final eventService = EventService();
+  //Danh sách sự kiện
+  List<EventModel> items = [];
+
+  //Taọ CalendarController để điều khiển sfCalendar
+  final calendarController = CalendarController();
+
+  @override
+  void initState() {
+    super.initState();
+    calendarController.view = CalendarView.day;
+    loadEvents();
+  }
+
+  Future<void> loadEvents() async {
+    final events = await eventService.getAllEvents();
+    setState(() {
+      items = events;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final al = AppLocalizations.of(context)!;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(al.appTitle),
+        actions: [
+          PopupMenuButton<CalendarView>(
+            onSelected: (value) {
+              setState(() {
+                calendarController.view = value;
+              });
+            },
+            itemBuilder: (context) => CalendarView.values.map((view) {
+              return PopupMenuItem<CalendarView>(
+                value: view,
+                child: ListTile(title: Text(view.name)),
+              );
+            }).toList(),
+            icon: getCalendarViewIcon(calendarController.view!),
+          ),
+          IconButton(
+            onPressed: () {
+              calendarController.displayDate = DateTime.now();
+            },
+            icon: const Icon(Icons.today_outlined),
+          ),
+          IconButton(onPressed: loadEvents, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: SfCalendar(
+        controller: calendarController,
+        dataSource: EventDataSource(items),
+        monthViewSettings: const MonthViewSettings(
+            appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+        //Nhấn giữ vào cell để thêm sự kiện
+        onLongPress: (details) {
+          //Khong co su kien trong cell
+          if (details.targetElement == CalendarElement.calendarCell) {
+            //Tạo một đối tượng sự kiện tại thời gian trong lịcg theo giao diện
+            final newEvent = EventModel(
+                startTime: details.date!,
+                endTime: details.date!.add(const Duration(hours: 1)),
+                subject: 'Sự kiện mới');
+            //Điều hướng và định tuyeens bằng cách đưa newEvent vào detail view
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) {
+                return EventDetailView(event: newEvent);
+              },
+            )).then((value) async {
+              //Sau khi pop o detail view
+              if (value == true) {
+                await loadEvents();
+              }
+            });
+          }
+        },
+        //Chạm vào sự kiện để xem và cập nhật
+        onTap: (details) {
+          //Khi touch vào sự kiện -> sửa hoặc xoá
+          if (details.targetElement == CalendarElement.appointment) {
+            final EventModel event = details.appointments!.first;
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) {
+                return EventDetailView(event: event);
+              },
+            )).then((value) async {
+              //Sau khi pop ow detail view
+              if (value == true) {
+                await loadEvents();
+              }
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  //Hàm lấy icon tương ứng với calendar view
+  Icon getCalendarViewIcon(CalendarView view) {
+    switch (view) {
+      case CalendarView.day:
+        return const Icon(Icons.calendar_view_day_outlined);
+      case CalendarView.week:
+        return const Icon(Icons.calendar_view_week_outlined);
+      case CalendarView.workWeek:
+        return const Icon(Icons.work_history_outlined);
+      case CalendarView.month:
+        return const Icon(Icons.calendar_view_month_outlined);
+      case CalendarView.schedule:
+        return const Icon(Icons.schedule_outlined);
+      default:
+        return const Icon(Icons.calendar_today_outlined);
+    }
+  }
+}
